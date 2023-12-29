dd7a86bd2b69712a2bb444cc434053929902874f
jlujan209 <jlujan209@gmail.com>
2023-12-28 18:02:44 -0700

    authentication

--- blackJackSite/blackjack/urls.py
+++ blackJackSite/blackjack/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("game", views.game, name="game")
+    path("game", views.game, name="game"), 
+    path("player/", views.PlayerList.as_view(), name = "playerlist"),
+    path("signup/", views.SignUp.as_view(), name="signup"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
 ]
--- blackJackSite/blackjack/views.py
+++ blackJackSite/blackjack/views.py
@@ -1,13 +1,49 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-####Testing setup
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from .models import Player
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
 
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def login_view(request):
+  context = {
+    "login_view": "active"
+  }
+  if request.method == "POST":
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      # Add your code below:
+      login(request, user)
+      return redirect("home")
+    else:
+      return HttpResponse("invalid credentials")
+  return render(request, "registration/login.html", context)
+    
+@login_required
 def home(request):
     return render(request, "blackjack/home.html")
 
+@login_required
 def game(request):
     return render(request, "blackjack/game.html")
+
+class PlayerList(LoginRequiredMixin,ListView):
+    model = Player
+    template_name = "blackjack/player_list.html"
